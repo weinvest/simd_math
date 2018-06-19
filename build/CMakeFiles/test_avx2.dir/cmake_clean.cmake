72e6642f9719de63b8c6af280e4f583ca986570c
shugan.li <shugan.li@dfctec.com>
2018-06-19 21:57:52 +0800

    [Normal]

---
+++ build/CMakeFiles/test_avx2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_avx2.dir/DoubleTest.cpp.o"
+  "CMakeFiles/test_avx2.dir/FloatTest.cpp.o"
+  "test_avx2.pdb"
+  "test_avx2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_avx2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
